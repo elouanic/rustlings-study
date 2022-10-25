ec6a45031ca2fa91f772efee7d403e1cdb54f01f
young <young@contxts.io>
2022-10-25 22:26:41 +0900

    variables5

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -6,6 +6,7 @@
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
+    let number: usize;
     number = 3; // don't rename this variable
     println!("Number plus two is : {}", number + 2);
 }
